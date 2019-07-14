3718d0a6efe02a0df7c98148dd1ca27f6f5ddd55
Gytis <gytis'718293@gmail.com>
2019-07-14 14:12:47 +0300

    separated CSS from template.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,36 +4,7 @@
     <title>Form</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <style>
-        .StripeElement {
-            box-sizing: border-box;
-
-            height: 40px;
-
-            padding: 10px 12px;
-
-            border: 1px solid transparent;
-            border-radius: 4px;
-            background-color: white;
-
-            box-shadow: 0 1px 3px 0 #e6ebf1;
-            -webkit-transition: box-shadow 150ms ease;
-            transition: box-shadow 150ms ease;
-        }
-
-        .StripeElement--focus {
-            box-shadow: 0 1px 3px 0 #cfd7df;
-        }
-
-        .StripeElement--invalid {
-            border-color: #fa755a;
-        }
-
-        .StripeElement--webkit-autofill {
-            background-color: #fefde5 !important;
-        }
-    </style>
+    <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 </head>
 <body>
 
