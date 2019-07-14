894f9ffc76970eb49a1f643a939314ae19b5ac15
Gytis <gytis'718293@gmail.com>
2019-07-14 16:43:51 +0300

    created logger function for payments.

--- app/Http/Controllers/api/PaymentController.php
+++ app/Http/Controllers/api/PaymentController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use GuzzleHttp\Exception\GuzzleException;
 use GuzzleHttp\Client;
+use Illuminate\Support\Facades\Storage;
 
 class PaymentController extends Controller
 {
@@ -38,7 +39,40 @@ class PaymentController extends Controller
             //something is wrong with payment - redirect to bad payment page.
         }
 
+        $this->logStripePaymentToFile($response);
 
+
+    }
+
+    /**
+     * Logs cURL response for Stripe payment to file.
+     * Filename structure for logFile Y-m-d_H:i:s_{payment_id}
+     * @param $response Response that was gotten from cURL request. Contains all necessary data that needs to be
+     * outputted to log file.
+     * @return filename of outputted file. File is stored in {project_root}/storage/app/stripe_payment_logs/ folder.
+     */
+    public function logStripePaymentToFile($response)
+    {
+        $current_time = date('Y-m-d_H:i:s');
+        $formed_array = $this->formStripePaymentResponseData($response);
+        $file_name = 'stripe_payment_logs/'.$current_time.'_'.$formed_array['id'].'.txt';
+        Storage::disk('local')->put($file_name, print_r($formed_array,true));
+        return $file_name;
+    }
+
+    public function formStripePaymentResponseData($response)
+    {
+        $decoded_response = json_decode($response->getBody()->getContents());
+
+        //taking only needed values
+        return [
+            'id' => $decoded_response->id,
+            'object' => $decoded_response->object,
+            'amount' => $decoded_response->amount,
+            'currency' => $decoded_response->currency,
+            'description' => $decoded_response->description,
+            'paid' => $decoded_response->paid,
+        ];
     }
 
 
